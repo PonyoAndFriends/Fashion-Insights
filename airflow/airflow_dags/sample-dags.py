dab7d841d8e062af7dcf6dff70f22fd18d339061
CoffeeIsNak <tjdqnzo@naver.com>
2024-12-30 23:00:13 +0900

    Update sample-dags.py

--- airflow/airflow_dags/sample-dags.py
+++ airflow/airflow_dags/sample-dags.py
@@ -1,33 +1,31 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.models import Variable
 from datetime import datetime, timedelta
 import time
 
-def example_task(**kwargs):
-    # 태스크에서 실행할 로직
-    print("Hello from Airflow Worker Pod! - python operator")
-    time.sleep(180)
-    print("Running on a KubernetesExecutor.")
+def test_python_operator(**kwargs):
+    print("PythonOperator Task started")
+    time.sleep(60)  # 60초 동안 대기, Pod 상태를 확인할 시간 제공
+    print("PythonOperator Task completed")
 
 default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
 }
 
 with DAG(
-    dag_id='example_kubernetes_executor_dag',
+    dag_id='test_python_operator_pod_creation',
     default_args=default_args,
     schedule_interval=None,
     start_date=datetime(2023, 12, 25),
     catchup=False,
 ) as dag:
 
-    task1 = PythonOperator(
-        task_id='example_task',
-        python_callable=example_task,
+    python_operator_task = PythonOperator(
+        task_id='test_python_operator',
+        python_callable=test_python_operator,
     )
