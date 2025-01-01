fbd829e61263adf3334d413001297f312ffbf119
CoffeeIsNak <tjdqnzo@naver.com>
2025-01-01 16:17:40 +0900

    fix: file path and volumes for krb5

--- airflow/airflow_dags/spark_s3_example_dag.py
+++ airflow/airflow_dags/spark_s3_example_dag.py
@@ -13,6 +13,22 @@ default_args = {
 # sprark 코드 파일 경로
 PYSPARK_FILE = "/opt/spark/jobs/s3_example.py"
 
+# 마운트
+volumes = [
+    {
+        "name": "krb5-config-volume",
+        "configMap": {"name": "krb5-config"},
+    }
+]
+volume_mounts = [
+    {
+        "name": "krb5-config-volume",
+        "mountPath": "/etc/krb5.conf",
+        "subPath": "krb5.conf",
+        "readOnly": True,
+    }
+]
+
 # DAG 정의
 with DAG(
     dag_id='spark_s3_example',
@@ -25,7 +41,7 @@ with DAG(
     # Spark 파드 실행
     spark_task = KubernetesPodOperator(
         namespace='airflow',
-        image='coffeeisnan/spark_test_image:9',
+        image='coffeeisnan/spark_test_image:10',
         cmds=["/opt/bitnami/spark/bin/spark-submit"],
         arguments=[
             '--master', 'k8s://https://kubernetes.default.svc',
@@ -34,12 +50,14 @@ with DAG(
             '--conf', 'spark.executor.instances=2',
             '--conf', 'spark.kubernetes.authenticate.driver.serviceAccountName=spark-service-account',
             '--conf', 'spark.jars.ivy=/tmp/.ivy2',  # Ivy 디렉터리 설정
-            '--conf', 'spark.kubernetes.container.image=coffeeisnan/spark_test_image:9',
+            '--conf', 'spark.kubernetes.container.image=coffeeisnan/spark_test_image:10',
             '--conf', 'spark.kubernetes.file.upload.path=/tmp/spark-uploads/',
             PYSPARK_FILE,  # Spark 애플리케이션
             's3a://source-bucket-hs/input-data.json',  # 입력 파일
             's3a://destination-bucket-hs/output-data.json'  # 출력 파일
         ],
+        volumes=volumes,
+        volume_mounts=volume_mounts,
         container_resources={
             "limit_memory": "4Gi",
             "limit_cpu": "2",
