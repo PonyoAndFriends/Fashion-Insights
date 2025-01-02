8252e7316e0b8c86f1e31f1bcf2607e9df8bbc0e
DongHee Kim <54052704+donghee-kim775@users.noreply.github.com>
2025-01-02 16:09:05 +0900

    Feat : 무신사 RankingRawData 적재 및 python script 파일 작성

---
+++ DockerImages/pythonscript/Musinsa_Ranking_RawData_EL.py
@@ -0,0 +1,60 @@
+import boto3
+import logging
+import requests
+import os
+import json
+from s3_validate import connect_s3, validate_and_upload_s3_file
+
+from datetime import datetime, timedelta
+
+import time, random
+
+import sys
+import shutil
+
+# s3 connect
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+region_name='ap-northeast-2'
+
+s3_client = connect_s3(aws_access_key_id, aws_secret_access_key, region_name)
+
+# url
+url = "https://api.musinsa.com/api2/hm/v2/pans/ranking/sections/199?"
+
+# headers
+headers = {
+    "accept": "application/json, text/plain, */*",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    "origin": "https://www.musinsa.com",
+    "referer": "https://www.musinsa.com/",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+}
+
+params = {
+    "storeCode" : "musinsa",
+    "period" : "DAILY",
+    "AGE_BAND" : "AGE_BAND_ALL"
+}
+
+# categoryCode
+categoryCode = ["103000", "002000", "001000"] # 카테고리가 늘어남에 따라 수정 필요
+today_date = datetime.now().strftime("%Y-%m-%d")
+
+if __name__ == "__main__":
+    arguments1 = sys.argv[1]
+    
+    params['gf'] =  arguments1
+    
+    for code in categoryCode:
+        response = requests.get(url, headers=headers, params=params)
+        if response.status_code == 200:
+            response_json = response.json()
+            print("response 200")
+        
+        bucket_name = 'project4-raw-data'
+        file_name = f"{today_date}/Musinsa/RankingData/{code}/musinsa_{params['gf']}_{code}.json"
+        
+        validate_and_upload_s3_file(s3_client, bucket_name, file_name, response_json)
+        print(file_name)
---
+++ airflow/airflow_dags/musinsa/Musinsa_Ranking_RawData_EL_DAG.py
@@ -0,0 +1,55 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.operators.dummy import DummyOperator
+
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+from kubernetes.client import models as k8s
+
+# DAG 기본 설정
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+Sexual_Dynamic_Params = [{
+    "SEXUAL" : "F"
+}, {
+    "SEXUAL" : "M"
+}, {
+    "SEXUAL" : "A"
+}
+]
+
+# DAG 정의
+with DAG(
+    dag_id='Musinsa_Ranking_RawData_EL_DAG',
+    default_args=default_args,
+    description='Example DAG using dynamic Python tasks in KubernetesExecutor',
+    schedule_interval=None,  # 수동 실행
+    start_date=days_ago(1),
+    catchup=False,
+    tags=['example', 'k8s', 'dynamic-python'],
+) as dag:
+
+    # Start task
+    start = DummyOperator(
+        task_id="start")
+
+    for i, sexual_dct in enumerate(Sexual_Dynamic_Params):
+        sexual_task = KubernetesPodOperator(
+            task_id=f'{sexual_dct["SEXUAL"]}_task',
+            name=f'{sexual_dct["SEXUAL"]}_task',
+            namespace='airflow',
+            image='ehdgml7755/project4-custom:latest',
+            cmds=['python', './pythonscript/Musinsa_Ranking_RawData_EL.py'],  # Execute the script from /scripts
+            arguments=[sexual_dct["SEXUAL"]],  # Pass task_number as an argument to the script
+            is_delete_operator_pod=True,  # Do not delete pod after completion
+            get_logs=True,
+        )
+        start >> sexual_task
