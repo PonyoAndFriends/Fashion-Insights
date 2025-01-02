58e86f4c63f7bc4d0799a4524a0fd1aecf4b75f1
DongHee Kim <54052704+donghee-kim775@users.noreply.github.com>
2025-01-03 01:09:33 +0900

    fix : PR 리뷰사항 반영

--- airflow/airflow_dags/musinsa/Musinsa_Ranking_RawData_EL_DAG.py
+++ airflow/airflow_dags/musinsa/Musinsa_Ranking_RawData_EL_DAG.py
@@ -9,15 +9,15 @@ from kubernetes.client import models as k8s
 
 # DAG 기본 설정
 default_args = {
-    'owner': 'airflow',
+    'owner': 'ehdgml7755@cu.ac.kr',
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retries': 24,
+    'retry_delay': timedelta(minutes=30),
 }
 
-Sexual_Dynamic_Params = [{
+SEXUAL_DYNAMIC_PARAMS = [{
     "SEXUAL" : "F"
 }, {
     "SEXUAL" : "M"
@@ -26,30 +26,56 @@ Sexual_Dynamic_Params = [{
 }
 ]
 
+AGE_BAND_DYNAMIC_PARAMS = [{
+    "AGE_BAND" : "AGE_BAND_ALL"
+}, {
+    "AGE_BAND" : "AGE_BAND_MINOR"
+}, {
+    "AGE_BAND" : "AGE_BAND_20"
+}, {
+    "AGE_BAND" : "AGE_BAND_25"
+}, {
+    "AGE_BAND" : "AGE_BAND_30"
+}, {
+    "AGE_BAND" : "AGE_BAND_35"
+}, {
+    "AGE_BAND" : "AGE_BAND_40"
+}
+]
+
 # DAG 정의
 with DAG(
     dag_id='Musinsa_Ranking_RawData_EL_DAG',
     default_args=default_args,
-    description='Example DAG using dynamic Python tasks in KubernetesExecutor',
-    schedule_interval=None,  # 수동 실행
+    description='musinsa ranking raw data extraction and loading to s3',
+    schedule_interval='0 0 * * *',
     start_date=days_ago(1),
     catchup=False,
-    tags=['example', 'k8s', 'dynamic-python'],
+    tags=['musinsa', 'ranking_rawdata', 'Extract', 'Load', 'S3', 'k8s']
 ) as dag:
 
     # Start task
     start = DummyOperator(
-        task_id="start")
-
-    for i, sexual_dct in enumerate(Sexual_Dynamic_Params):
-        sexual_task = KubernetesPodOperator(
-            task_id=f'{sexual_dct["SEXUAL"]}_task',
-            name=f'{sexual_dct["SEXUAL"]}_task',
-            namespace='airflow',
-            image='ehdgml7755/project4-custom:latest',
-            cmds=['python', './pythonscript/Musinsa_Ranking_RawData_EL.py'],  # Execute the script from /scripts
-            arguments=[sexual_dct["SEXUAL"]],  # Pass task_number as an argument to the script
-            is_delete_operator_pod=True,  # Do not delete pod after completion
-            get_logs=True,
+                task_id="start"
+            )
+    
+    for i, sexual_dct in enumerate(SEXUAL_DYNAMIC_PARAMS):
+        sexual_task = DummyOperator(
+                        task_id=f'{sexual_dct["SEXUAL"]}_task'
         )
+        
         start >> sexual_task
+        
+        for j, age_band_dct in enumerate(AGE_BAND_DYNAMIC_PARAMS):
+            ageband_task = KubernetesPodOperator(
+                                task_id=f'{sexual_dct["SEXUAL"]}_{age_band_dct["AGE_BAND"]}_task',
+                                name=f'{sexual_dct["SEXUAL"]}_{age_band_dct["AGE_BAND"]}_task',
+                                namespace='airflow',
+                                image='ehdgml7755/project4-custom:latest',
+                                cmds=['python', './pythonscript/musinsa_ranking_rawdata_el.py'],
+                                arguments=[sexual_dct["SEXUAL"], age_band_dct["AGE_BAND"]],
+                                is_delete_operator_pod=False,
+                                get_logs=True,
+            )
+            
+            sexual_task >> ageband_task
