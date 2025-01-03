d7f4924b818694fc9f5dc4a6c66d754d2799db4f
CoffeeIsNak <tjdqnzo@naver.com>
2025-01-03 21:06:35 +0900

    chore: musinsa_review_detail 초본

---
+++ DockerImages/pythonscript/musinsa_review_datail.py
@@ -0,0 +1,62 @@
+import requests
+import json
+import os
+import logging  # 추후 로깅을 추가하십시오
+import argparse
+
+from s3_validate import connect_s3, validate_and_upload_s3_file
+from datetime import datetime
+
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+region_name='ap-northeast-2'
+
+headers = {
+    "accept": "application/json, text/plain, */*",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    "origin": "https://www.musinsa.com",
+    "referer": "https://www.musinsa.com/",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+}
+
+base_url = f"https://goods.musinsa.com/api2/review/v1/view/list"
+
+today_date = datetime.now().strftime("%Y-%m-%d")
+
+
+def main():
+    # argparse를 활용하여 argument를 가져옴.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gender", type=str, required=True)
+    parser.add_argument("--category", type=str, required=True)
+    parser.add_argument("--product_ids", type=str, required=True)
+
+    args = parser.parse_args()
+
+    # product_ids 문자열을 리스트로 변환
+    product_ids = json.loads(args.product_ids)
+    s3_client = connect_s3(aws_access_key_id, aws_secret_access_key, region_name)
+
+    for product_id in product_ids:
+        
+        params = {
+            "page": 0,
+            "pageSize": 10,
+            "goodsNo": f"{product_id}",
+            "sort": "new",
+            "selectedSimilarNo": "1551840",
+            "myFilter": "false",
+            "hasPhoto": "false",
+            "isExperience": "false",
+        }
+        response = requests.get(base_url, params=params, headers=headers)
+        response.raise_for_status()
+        
+        bucket_name = 'source-bucket-hs'
+        file_name = f"{today_date}/Musinsa/review_detail/{args.gender}/{args.category}/{agrs.gender}_{args.category}_review_detail.json"
+        
+        validate_and_upload_s3_file(s3_client, bucket_name, file_name, response.json())
+
+if __name__=="__main__":
+    main()
---
+++ airflow/airflow_dags/musinsa/musinsa_review_detail_dag.py
@@ -0,0 +1,75 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime
+import math
+
+# 병렬 실행할 태스크의 개수
+PARALLEL_TASK_NUM = 5
+
+# 적당히 알맞게 수정 필요
+default_args = {
+    "owner": "dongheekim@mola.com",
+    "start_date": datetime(2024, 1, 1),
+    "retries": 1,
+}
+
+def split_product_ids(**context):
+    """
+    product id의 리스트를 적절하게 나누는 함수 => 파이썬 오퍼레이터로 airflow worker 내에서 실행 (간단한 로직)
+    """
+    # 이전 대그에서 전달받는 성별, 카테고리, 프로덕트 id 리스트(약 50개를 가정)
+    gender = context["dag_run"].conf.get("gender", "unknown")  # 'M', 'F', or 'A'
+    category = context["dag_run"].conf.get("category", "unknown")  # e.g., '103000'
+    product_ids = context["dag_run"].conf.get("product_ids", [])  # List of 50 product IDs
+
+    # 5개의 
+    chunk_size = math.ceil(len(product_ids) / PARALLEL_TASK_NUM)  # 
+    product_id_chunks = [product_ids[i:i + chunk_size] for i in range(0, len(product_ids), chunk_size)]
+
+    # Push data to XCom for downstream tasks
+    context["ti"].xcom_push(key="gender", value=gender)
+    context["ti"].xcom_push(key="category", value=category)
+    context["ti"].xcom_push(key="product_id_chunks", value=product_id_chunks)
+
+
+# Define the DAG
+with DAG(
+    dag_id="dynamic_k8s_pod_parallel_processing",
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+) as dag:
+
+    # Step 1: Split product IDs
+    split_ids_task = PythonOperator(
+        task_id="split_product_ids",
+        python_callable=split_product_ids,
+        provide_context=True,
+    )
+
+    def create_pod_task(index, chunk):
+        return KubernetesPodOperator(
+            task_id=f"fetch_review_datail_task_{index}",
+            namespace="airflow",
+            image="coffeeisnan/project4-custom:latest",
+            cmds=["python", "./pythonscript/musinsa_review_detail.py"],
+            arguments=[
+                f"--gender={{{{ ti.xcom_pull(task_ids='split_product_ids', key='gender') }}}}",
+                f"--category={{{{ ti.xcom_pull(task_ids='split_product_ids', key='category') }}}}",
+                f"--product_ids={chunk}",
+            ],
+            name=f"fetch_review_datail_pod_{index}",
+            is_delete_operator_pod=True,
+            in_cluster=True,
+        )
+
+    # Dynamically create tasks for each chunk
+    for i in range(1, PARALLEL_TASK_NUM + 1):
+        pod_task = create_pod_task(
+            index=i,
+            chunk="{{ ti.xcom_pull(task_ids='split_product_ids', key='product_id_chunks')["
+            + str(i)
+            + "] }}",
+        )
+        split_ids_task >> pod_task
