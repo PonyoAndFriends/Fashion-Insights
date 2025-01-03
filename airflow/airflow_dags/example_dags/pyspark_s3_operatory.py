abe86e5b7d3c2dcd240576d5e0f673a0d7ad7875
CoffeeIsNak <tjdqnzo@naver.com>
2025-01-03 17:14:22 +0900

    fix: pyspark jars path

--- airflow/airflow_dags/example_dags/pyspark_s3_operatory.py
+++ airflow/airflow_dags/example_dags/pyspark_s3_operatory.py
@@ -55,8 +55,8 @@ def submit_spark_application():
             },
             "deps": {
                 "jars": [
-                    "local:///opt/spark/jars/hadoop-aws-3.3.1.jar",
-                    "local:///opt/spark/jars/aws-java-sdk-bundle-1.11.901.jar",
+                    "local:///opt/spark/user-jars/hadoop-aws-3.3.1.jar",
+                    "local:///opt/spark/user-jars/aws-java-sdk-bundle-1.11.901.jar",
                 ]
             },
         },
