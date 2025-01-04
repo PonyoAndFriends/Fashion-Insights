c1ec0b65a92647a9d8078af8b9d7a82152dd9b9c
CoffeeIsNak <tjdqnzo@naver.com>
2025-01-04 09:32:01 +0900

    chore: linting apply

--- DockerImages/pythonscript/musinsa_review_datail.py
+++ DockerImages/pythonscript/musinsa_review_datail.py
@@ -7,9 +7,9 @@ import argparse
 from s3_validate import connect_s3, validate_and_upload_s3_file
 from datetime import datetime
 
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-region_name='ap-northeast-2'
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+region_name = "ap-northeast-2"
 
 headers = {
     "accept": "application/json, text/plain, */*",
@@ -17,7 +17,7 @@ headers = {
     "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
     "origin": "https://www.musinsa.com",
     "referer": "https://www.musinsa.com/",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
 }
 
 base_url = f"https://goods.musinsa.com/api2/review/v1/view/list"
@@ -39,7 +39,6 @@ def main():
     s3_client = connect_s3(aws_access_key_id, aws_secret_access_key, region_name)
 
     for product_id in product_ids:
-        
         params = {
             "page": 0,
             "pageSize": 10,
@@ -52,11 +51,12 @@ def main():
         }
         response = requests.get(base_url, params=params, headers=headers)
         response.raise_for_status()
-        
-        bucket_name = 'source-bucket-hs'
+
+        bucket_name = "source-bucket-hs"
         file_name = f"{today_date}/Musinsa/review_detail/{args.gender}/{args.category}/{agrs.gender}_{args.category}_review_detail.json"
-        
+
         validate_and_upload_s3_file(s3_client, bucket_name, file_name, response.json())
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
--- airflow/airflow_dags/musinsa/musinsa_review_detail_dag.py
+++ airflow/airflow_dags/musinsa/musinsa_review_detail_dag.py
@@ -1,6 +1,8 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
 from datetime import datetime
 import math
 
@@ -14,6 +16,7 @@ default_args = {
     "retries": 1,
 }
 
+
 def split_product_ids(**context):
     """
     product id의 리스트를 적절하게 나누는 함수 => 파이썬 오퍼레이터로 airflow worker 내에서 실행 (간단한 로직)
@@ -21,11 +24,15 @@ def split_product_ids(**context):
     # 이전 대그에서 전달받는 성별, 카테고리, 프로덕트 id 리스트(약 50개를 가정)
     gender = context["dag_run"].conf.get("gender", "unknown")  # 'M', 'F', or 'A'
     category = context["dag_run"].conf.get("category", "unknown")  # e.g., '103000'
-    product_ids = context["dag_run"].conf.get("product_ids", [])  # List of 50 product IDs
+    product_ids = context["dag_run"].conf.get(
+        "product_ids", []
+    )  # List of 50 product IDs
 
-    # 5개의 
-    chunk_size = math.ceil(len(product_ids) / PARALLEL_TASK_NUM)  # 
-    product_id_chunks = [product_ids[i:i + chunk_size] for i in range(0, len(product_ids), chunk_size)]
+    # 5개의
+    chunk_size = math.ceil(len(product_ids) / PARALLEL_TASK_NUM)  #
+    product_id_chunks = [
+        product_ids[i : i + chunk_size] for i in range(0, len(product_ids), chunk_size)
+    ]
 
     # Push data to XCom for downstream tasks
     context["ti"].xcom_push(key="gender", value=gender)
@@ -37,10 +44,9 @@ def split_product_ids(**context):
 with DAG(
     dag_id="dynamic_k8s_pod_parallel_processing",
     default_args=default_args,
-    schedule_interval='@daily',
+    schedule_interval="@daily",
     catchup=False,
 ) as dag:
-
     # Step 1: Split product IDs
     split_ids_task = PythonOperator(
         task_id="split_product_ids",
