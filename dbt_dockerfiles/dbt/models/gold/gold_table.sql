764917112b1db7f81567268fecc44213faebe236
CoffeeIsNak <tjdqnzo@naver.com>
2025-01-08 23:59:00 +0900

    fix: delete dbt project and actions

--- dbt_dockerfiles/dbt/models/gold/gold_table.sql
+++
@@ -1,9 +0,0 @@
-WITH aggregated_data AS (
-    SELECT
-        column1,
-        COUNT(column2) AS record_count,
-        MAX(processed_date) AS latest_date
-    FROM {{ ref('silver_table') }}
-    GROUP BY column1
-)
-SELECT * FROM aggregated_data;
--- dbt_dockerfiles/dbt/models/silver/silver_table.sql
+++
@@ -1,9 +0,0 @@
-WITH cleaned_data AS (
-    SELECT
-        column1,
-        column2,
-        column3,
-        CAST(column4 AS DATE) AS processed_date
-    FROM {{ ref('staging_table') }}
-)
-SELECT * FROM cleaned_data;
