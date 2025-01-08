512328eb9d6660401581ec98c4b6eac0782cf5ca
CoffeeIsNak <tjdqnzo@naver.com>
2025-01-09 05:04:31 +0900

    feat: 골드 테이블까지 전체  대그 초안

---
+++ airflow/airflow_dags/redshift_dags/gold_dags/naver_keyword_trend_gold_table_dag.py
@@ -0,0 +1,51 @@
+from airflow import DAG
+from airflow.models import Variable
+from datetime import datetime
+from custom_sql_operators.custom_query_operator import RedshiftQueryOperator
+from custom_sql_operators.custom_refresh_table_operator import RefreshTableOperator
+from custom_sql_modules.query_dag_dependencies import (
+    SILVER_LOAD_DEFAULT_ARGS,
+    DEFAULT_SILVER_SHCEMA,
+    DEFAULT_GOLD_SHCEMA,
+    NOW_STRING,
+    DEFULAT_SILVER_BUCKET_URL,
+)
+
+# DAG 기본 설정
+default_args = SILVER_LOAD_DEFAULT_ARGS
+
+# DAG 정의
+with DAG(
+    dag_id="naver_keyword_trend_gold_dag",
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+) as dag:
+
+    # 기본적인 설정 정의
+    table = "naver_shopping_kwd_tb"
+    redshift_iam_role = Variable.get("redshift_iam_role")
+
+    drop_sql = f"""
+    DROP TABLE IF EXIST {DEFAULT_GOLD_SHCEMA}.{table};
+    """
+
+    create_sql = f"""
+    CREATE TABLE {DEFAULT_GOLD_SHCEMA}.{table} AS
+    SELECT
+        keyword_name,
+        ratio,
+        TO_CHAR(period, 'MM-DD') AS period,
+        created_at
+    FROM
+        {DEFAULT_SILVER_SHCEMA}.{table};
+    """
+
+    full_refresh_task = RefreshTableOperator(
+        drop_sql,
+        create_sql,
+        task_id="naver_keyword_trend_gold_table_task",
+    )
+
+    full_refresh_task
