bc252aff637609d74a30b08634f3acbd32fec616
DongHee Kim <54052704+donghee-kim775@users.noreply.github.com>
2025-01-09 20:11:43 +0900

    feat : musinsa - base complete

---
+++ DockerImages/pythonscript/musinsa/musinsa_productdetail_rawdata_etl.py
@@ -0,0 +1,154 @@
+import time
+import argparse
+import requests
+import re
+from datetime import datetime, timedelta
+import json
+import threading
+
+from bs4 import BeautifulSoup
+
+from modules.config import Musinsa_Config
+import modules.s3_module as s3_module
+
+TODAY_DATE = Musinsa_Config.TODAY_DATE
+
+LIST_SIZE = 10
+
+def porductid_list_iterable(iterable):
+    for i in range(0, len(iterable), LIST_SIZE):
+        yield iterable[i:i + LIST_SIZE]
+
+def mapping_2depth_kor(depth2category):
+    if depth2category == 'top':
+        return '상의'
+    elif depth2category == 'bottom':
+        return '하의'
+    elif depth2category == 'shoes':
+        return '신발'
+    elif depth2category == 'outer':
+        return '아우터'
+
+# parsing 안되는 경우
+def get_text_or_none(element):
+    return element.text if element else None
+
+def get_content_or_none(element):
+    return element['content'] if element else None
+
+def et_product1_detail(product_id):
+    url = f"https://www.musinsa.com/products/{product_id}"
+    
+    response = requests.get(url, headers=Musinsa_Config.HEADERS)
+    
+    soup = BeautifulSoup(response.text, features="html.parser")
+
+    title_text = soup.find('title').text
+    product_name = re.sub(r' - 사이즈 & 후기.*', '', title_text)
+
+    # brand naeme_kr, brand name_en
+    brand_name_kr = soup.find('meta', {'property': 'product:brand'}).get('content')
+    brand_name_en = soup.find('div', class_='sc-11x022e-0 hzZrPp')
+    brand_name_en = brand_name_en.find('a', href=True)['href'].split('brand/')[1] if brand_name_en else None
+
+    original_price = get_content_or_none(soup.find('meta', {'property': 'product:price:normal_price'}))
+    final_price = get_content_or_none(soup.find('meta', {'property': 'product:price:amount'}))
+    discount_rate = get_content_or_none(soup.find('meta', {'property': 'product:price:sale_rate'}))
+
+    try:
+        # review_count, review_avg_ratin
+        json_data = json.loads(soup.find('script', {'type': 'application/ld+json'}).string)
+        # ratingValue와 reviewCount 값 추출
+        review_count = json_data['aggregateRating']['reviewCount']
+        review_avg_rating = json_data['aggregateRating']['ratingValue']
+    except:
+        review_count = None
+        review_avg_rating = None
+    
+    image_tag = soup.find('meta', attrs={'property': 'og:image'})['content']
+    
+    return product_name, brand_name_kr, brand_name_en, original_price, final_price, discount_rate, review_count, review_avg_rating, image_tag
+
+def et_product2_detail(product_id):
+    url = f"https://like.musinsa.com/like/api/v2/liketypes/goods/counts"
+
+    payload = {
+        "relationIds": [product_id]
+    }
+    
+    try:
+        response = requests.post(url, headers=Musinsa_Config.HEADERS, json=payload).json()
+
+        like_counting = response['data']['contents']['items'][0]['count']
+    except:
+        like_counting = None
+
+    return like_counting
+
+# product detail parsing => DataFrame Record
+def et_product_detail(master_category, depth4category, product_id_list, key):
+    for product_id in product_id_list:
+        bronze_bucket = "project4-raw-data"
+        s3_key = key + f"{product_id}.json"
+        time.sleep(0.8)
+        
+        print(f"product_id : {product_id}")
+        
+        # request api
+        product_name, brand_name_kr, brand_name_en, original_price, final_price, discount_rate, review_count, review_avg_rating, image_tag = et_product1_detail(product_id)
+        like_counting = et_product2_detail(product_id)
+        
+        created_at = datetime.now().strftime('%Y-%m-%d')
+        
+        # data => dict
+        data = {
+            "platform": 'Musinsa',
+            "master_category_name": master_category,
+            "small_category_name": depth4category,
+            "product_id": product_id,
+            "product_name": product_name,
+            "brand_name_kr": brand_name_kr,
+            "brand_name_en": brand_name_en,
+            "original_price": original_price,
+            "final_price": final_price,
+            "discount_rate": discount_rate,
+            "review_count": review_count,
+            "review_avg_rating": review_avg_rating,
+            "like_counting": like_counting,
+            "image_src" : image_tag,
+            "created_at": created_at,
+        }
+        
+        s3_module.upload_json_to_s3(bronze_bucket, s3_key, data)
+    
+def main():
+    # argment parsing
+    parser = argparse.ArgumentParser(description="sexual/category param")
+    parser.add_argument('sexual', type=str, help='sexual')
+    parser.add_argument('category_data', type=str, help='category')
+    
+    args = parser.parse_args()
+
+    sexual_data = json.loads(args.sexual)
+    category_data = json.loads(args.category_data)
+    
+    category2depth = mapping_2depth_kor(category_data[0])
+    
+    for category_info in category_data[1]:
+        category3depth = list(category_info.items())[0]
+        
+        for category4depth in category3depth[1].values():
+            silver_bucket = "project4-silver-data"
+            read_file_path = f"{TODAY_DATE}/Musinsa/RankingData/{category3depth[0]}/{sexual_data[1]}_{category2depth}_{category3depth[0]}_{category4depth}.parquet"
+            file_path = f"{silver_bucket}/{read_file_path}"
+            product_lists = s3_module.get_product_ids(file_path)
+            
+            for product_list in porductid_list_iterable(product_lists):
+                master_category = f"{sexual_data[1]}-{category2depth}-{category3depth[0]}"
+                key = f"{TODAY_DATE}/Musinsa/ProductDetailData/{category3depth[0]}/{category4depth}/"
+                t = threading.Thread(target=et_product_detail, args = (master_category, category4depth, product_list, key))
+                t.start()
+            
+if __name__ == "__main__":
+    main()
+    
--- spark/spark_job/musinsa/musinsa_ranking_silverdata_spark.py
+++ spark/spark_job/musinsa/musinsa_ranking_silverdata_spark.py
@@ -1,27 +1,32 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, FloatType, IntegerType, DateType
+from pyspark.sql.functions import to_date
 from pyspark.sql.functions import col, explode, lit
 
 from pyspark.conf import SparkConf
 from datetime import datetime
 
 import os
+import pendulum
 
-from musinsa_mappingtable import SEXUAL_CATEGORY_DYNAMIC_PARAMS, mapping_2depth_kor
+from modules.musinsa_mappingtable import SEXUAL_CATEGORY_DYNAMIC_PARAMS, mapping_2depth_kor
 
+# 오늘 날짜 - 날짜 path
+LOCAL_TZ = pendulum.timezone("Asia/Seoul")
+    
+TODAY_DATE = pendulum.now(tz=LOCAL_TZ).to_date_string()
 
 def create_spark_session():
     # SparkConf 설정
     conf = SparkConf()
+    conf.set("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version", 2)
+    conf.set("spark.hadoop.fs.s3a.committer.magic.enabled", "true")
+    conf.set("fs.s3a.committer.name", "magic")
     conf.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
     conf.set("spark.hadoop.fs.s3a.access.key", os.getenv("AWS_ACCESS_KEY_ID"))
     conf.set("spark.hadoop.fs.s3a.secret.key", os.getenv("AWS_SECRET_ACCESS_KEY"))
-    conf.set(
-        "spark.hadoop.fs.s3a.endpoint", "s3.ap-northeast-2.amazonaws.com"
-    )  # 리전 엔드포인트 수정
-    conf.set(
-        "spark.hadoop.fs.s3a.aws.credentials.provider",
-        "com.amazonaws.auth.DefaultAWSCredentialsProviderChain",
-    )
+    conf.set("spark.hadoop.fs.s3a.endpoint", "s3.ap-northeast-2.amazonaws.com")  # 리전 엔드포인트 수정
+    conf.set("spark.hadoop.fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
     conf.set("spark.executor.memory", "4g")
     conf.set("spark.driver.memory", "2g")
     conf.set("spark.sql.parquet.compression.codec", "snappy")  # Parquet 파일 압축 설정
@@ -29,54 +34,46 @@ def create_spark_session():
     spark = SparkSession.builder.config(conf=conf).getOrCreate()
     return spark
 
-
 def making_ranking_table(spark, json_path, master_category_code, today_date):
     df = spark.read.json(json_path)
     items_df = None
     for i in range(1, 18):
-        temp_df = df.select(
-            explode(col("data.modules")[i]["items"]).alias("item")
-        ).select(
-            col("item.id").cast("bigint").alias("product_id"),
-            col("item.info.onClickBrandName.eventLog.ga4.payload.index")
-            .cast("int")
-            .alias("ranking"),
+        temp_df = (
+            df.select(explode(col("data.modules")[i]["items"]).alias("item"))
+            .select(
+                col("item.id").cast("int").alias("product_id"),
+                col("item.info.onClickBrandName.eventLog.ga4.payload.index").cast("int").alias("ranking")
+            )
         )
-        temp_df = temp_df.withColumn(
-            "category", lit(master_category_code).cast("string")
-        )
+        temp_df = temp_df.withColumn("master_category_name", lit(master_category_code).cast("string"))
         temp_df = temp_df.withColumn("platform", lit("musinsa").cast("string"))
-        temp_df = temp_df.withColumn("date", lit(today_date).cast("string"))
-
+        temp_df = temp_df.withColumn("created_at", to_date(lit(today_date), 'yyyy-MM-dd'))
+        
         if items_df is None:
             items_df = temp_df
         else:
             items_df = items_df.union(temp_df)
-
+    
     items_df = items_df.dropna()
-
+    
     # 해당 Dataframe을 쓸 때, parquet 크기가 128MB가 안넘어가기에, 1개의 파일로 저장
     items_df = items_df.coalesce(1)
-
+    
     return items_df
-
 
 def main():
     spark = create_spark_session()
 
-    # 오늘 날짜 - 날짜 path
-    today_date = (datetime.now()).strftime("%Y-%m-%d")
-
     for sexual_dct in SEXUAL_CATEGORY_DYNAMIC_PARAMS:
         # category1depth(성별) 추출
-        sexual = list(sexual_dct["SEXUAL"].items())[0][1]
-
-        category = sexual_dct["CATEGORIES"]
+        sexual = list(sexual_dct['SEXUAL'].items())[0][1]
+        
+        category = sexual_dct['CATEGORIES']
         # category2depth 추출
         for categorydepth in category:
             categories = list(categorydepth.items())[0]
             category2depth = mapping_2depth_kor(categories[0])
-
+            
             # category3depth 추출
             for detailcategories in categories[1]:
                 detail_category = list(detailcategories.items())[0]
@@ -85,27 +82,22 @@ def main():
                 # category4depth 추출
                 for detail_category4 in detail_category[1].values():
                     category4depth = detail_category4
-
+                    
                     # 공통 path
                     file_name = f"{category3depth}/{sexual}_{category2depth}_{category3depth}_{category4depth}"
-
+                    
                     # input - filepath 조합
-                    input_path = f"s3a://project4-raw-data/{today_date}/Musinsa/RankingData/{file_name}.json"
-
+                    input_path = f"s3a://project4-raw-data/{TODAY_DATE}/Musinsa/RankingData/{file_name}.json"
+                
                     # output - filepath 조합
-                    output_path = f"s3a://project4-silver-data/{today_date}/Musinsa/RankingData/{file_name}.parquet"
-
+                    table_output_path = f"s3a://project4-silver-data/{TODAY_DATE}/Musinsa/RankingData/{file_name}.parquet"
+                    
                     master_category_code = f"{sexual}-{category2depth}-{category3depth}"
-
-                    sexualeng = (
-                        "F" if sexual == "여성" else "M" if sexual == "남성" else None
-                    )
-
-                    cleaned_df = making_ranking_table(
-                        spark, input_path, master_category_code, today_date
-                    )
-                    cleaned_df.write.mode("overwrite").parquet(output_path)
-
-
+                    print(f"Processing {master_category_code}-{category4depth}")
+                    cleaned_df = making_ranking_table(spark, input_path ,master_category_code, TODAY_DATE)
+                    
+                    cleaned_df.write.mode("overwrite").parquet(table_output_path)
+                    
 if __name__ == "__main__":
     main()
+    
