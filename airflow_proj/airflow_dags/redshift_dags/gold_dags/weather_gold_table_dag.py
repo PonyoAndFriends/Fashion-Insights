3d52ab492e90e55f37d1980246bd7993a142bd31
CoffeeIsNak <tjdqnzo@naver.com>
2025-01-10 16:28:11 +0900

    fix: 경로 수정 및 테스팅

--- airflow_proj/airflow_dags/redshift_dags/gold_dags/weather_gold_table_dag.py
+++ airflow_proj/airflow_dags/redshift_dags/gold_dags/weather_gold_table_dag.py
@@ -1,9 +1,9 @@
 from airflow import DAG
 from datetime import datetime
-from airflow_proj.airflow_dags.redshift_dags.custom_sql_operators.custom_refresh_table_operator import (
+from redshift_dags.custom_sql_operators.custom_refresh_table_operator import (
     RefreshTableOperator,
 )
-from airflow_proj.airflow_dags.redshift_dags.custom_sql_modules.query_dag_dependencies import (
+from redshift_dags.custom_sql_modules.query_dag_dependencies import (
     SILVER_LOAD_DEFAULT_ARGS,
     DEFAULT_SILVER_SHCEMA,
     DEFAULT_GOLD_SHCEMA,
