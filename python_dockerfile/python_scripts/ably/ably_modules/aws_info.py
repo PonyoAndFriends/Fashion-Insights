ed6f3fce007ee77a5317ad1ae333d790e6bb441b
CoffeeIsNak <tjdqnzo@naver.com>
2025-01-13 12:02:06 +0900

    fix: modules

--- python_dockerfile/python_scripts/ably/ably_modules/aws_info.py
+++ python_dockerfile/python_scripts/ably/ably_modules/aws_info.py
@@ -1,10 +1,10 @@
-from airflow.models import Variable
+import os
 
 # AWS S3 설정
-AWS_ACCESS_KEY = Variable.get("aws_access_key_id")
-AWS_SECRET_KEY = Variable.get("aws_secret_access_key")
-BUCKET_NAME = Variable.get("s3_bucket")
-REGION = Variable.get("aws_region")
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+BUCKET_NAME = "team3-2-s3"
+REGION = "ap-northeast-2"
 
 # S3 접속 정보를 딕셔너리로 정의
 AWS_S3_CONFIG = {
